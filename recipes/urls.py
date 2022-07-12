e698ff2393e302d496d453b33c0f1b3eff1a8637
Adrian Kauling <adriankauling4@gmail.com>
2022-07-11 22:10:59 -0300

    Using templates in Django (2)

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,3 +1,4 @@
+
 from django.urls import path
 
 from recipes.views import about, contact, home
--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('That\'s my first view with django')
+    return render(request, 'recipes/home.html')
 
 
 def contact(request):
